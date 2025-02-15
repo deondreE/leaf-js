204625a61bb5c342212fdf4242fc2f6dc10169bf
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2025-02-15 11:22:04 -0500

    Back to parsing

--- src/parsers/aseprite.ts
+++ src/parsers/aseprite.ts
@@ -19,42 +19,41 @@ export interface AseHeader {
   //read past the next 3 bytes
   pixelWidth: number; //byte
   pixelHeight: number; //byte
-  x: number //short
-  y: number //short
-  gridCell: number // word (described as grid with but each cell is square so this represents the size of a cell)
+  x: number; //short
+  y: number; //short
+  gridCell: number; // word (described as grid with but each cell is square so this represents the size of a cell)
   //read the next 84 bytes
 }
 
-export interface AseFrame{
-	size: number; //dword (number of bytes in the frame)
-	magic: number; //word should always be 0xF1FA
-	chunksOld: number; //word (represents the number of chunks in the frame if overflow there are more chunks in the frame that cant be described in this field and should use new field. 0xFFFF is max).
-	duration:number; //word (in ms)
-	//skip next byte[2]
-	chunksNew: number; //dword (if 0 check old field)
+export interface AseFrame {
+  size: number; //dword (number of bytes in the frame)
+  magic: number; //word should always be 0xF1FA
+  chunksOld: number; //word (represents the number of chunks in the frame if overflow there are more chunks in the frame that cant be described in this field and should use new field. 0xFFFF is max).
+  duration: number; //word (in ms)
+  //skip next byte[2]
+  chunksNew: number; //dword (if 0 check old field)
 }
 
 export interface AseFrameChunk {
-	size: number; //dword
-	type: number; //todo map to enum
+  size: number; //dword
+  type: number; //todo map to enum
 }
 
 export enum AseChunkType {
-	oldPalette1 = 0x0004, //should only be used if newPalette does not also exist.
-	oldPalette2 = 0x0011, //ignore if newPallet exists
-	layer = 0x2004,
-	cel = 0x2005,
-	celExtra = 0x2006,
-	colorProfile = 0x2007,
-	external = 0x2008,
-	mask = 0x2016, //deprecated
-	pth = 0x2017, //never used
-	tags = 0x2018,
-	palette = 0x2019, //newPalette
-	userData = 0x2020, //data to be appended 
-	slice = 0x2022, //slice frame
-	tileset = 0x2023, //tileset chunk
-
+  oldPalette1 = 0x0004, //should only be used if newPalette does not also exist.
+  oldPalette2 = 0x0011, //ignore if newPallet exists
+  layer = 0x2004,
+  cel = 0x2005,
+  celExtra = 0x2006,
+  colorProfile = 0x2007,
+  external = 0x2008,
+  mask = 0x2016, //deprecated
+  pth = 0x2017, //never used
+  tags = 0x2018,
+  palette = 0x2019, //newPalette
+  userData = 0x2020, //data to be appended
+  slice = 0x2022, //slice frame
+  tileset = 0x2023, //tileset chunk
 }
 
 /*
@@ -71,38 +70,92 @@ That being said the legacy variations can be represented by a [3 or 4]Uint8
 */
 export type AseLegacyPaletteChunkItem = Uint8Array;
 export type AseLegacyPaletteChunk = {
-	size: number, 
-	items: AseLegacyPaletteChunkItem[],
+  size: number;
+  items: AseLegacyPaletteChunkItem[];
 };
 
 export type AsePaletteChunk = {
-	size: number,
-}
+  size: number;
+};
 
 export type AseLayerChunk = {
-	flags: number, //word 1=visible 2=editable 4=lockmovement 8=background 16=prefer-linked-cels 32=The layer should be displayed collapsed 64=The layer is a reference layer
-	type: number, //word 0=Normal (image) 1=group 2=tilemap
-	layer: number, //word numeric indication of layer depths
-	//width: number, //word (currently ignored)
-	//height: number, //word (also ignored)
-	blendMode: number, //word (pixel addition effect)
-	opacity: number, //byte (header must have the opacity flag (1))
-	//[3]byte
-	name: string, //string
-	tilesetIndex?: number, //dword - tileset index (only applicable to groups)
+  flags: number; //word 1=visible 2=editable 4=lockmovement 8=background 16=prefer-linked-cels 32=The layer should be displayed collapsed 64=The layer is a reference layer
+  type: number; //word 0=Normal (image) 1=group 2=tilemap
+  layer: number; //word numeric indication of layer depths
+  //width: number, //word (currently ignored)
+  //height: number, //word (also ignored)
+  blendMode: number; //word (pixel addition effect)
+  opacity: number; //byte (header must have the opacity flag (1))
+  //[3]byte
+  name: string; //string
+  tilesetIndex?: number; //dword - tileset index (only applicable to groups)
 };
 
 export type AseCelChunk = {
-	layerIndex: number, //word
-	x: number, //short
-	y: number, //short
-	alpha: number, //byte
-	type: number, //word 0-raw image (deprecated) 1-linked cel 2-CompressedImage 3- Compressed Tilemap
-	zIndex: number, //short 0 - default ordering +N - show this cell nlayers later -N show this cell N layers back.
-	//skip [5]bytes
-}
+  layerIndex: number; //word
+  x: number; //short
+  y: number; //short
+  alpha: number; //byte
+  type: number; //word 0-raw image (deprecated) 1-linked cel 2-CompressedImage 3- Compressed Tilemap
+  zIndex: number; //short 0 - default ordering +N - show this cell nlayers later -N show this cell N layers back.
+  //skip [5]bytes
+};
 
 export type AseCelChunkRawImage = AseCelChunk & {
-	width: number, //word 
-	height: number, //word
+  width: number; //word
+  height: number; //word
+};
+
+export class Aseprite {
+  private buffer: ArrayBuffer;
+  private view: DataView;
+  private offset: number = 0;
+
+  constructor(buffer: ArrayBuffer) {
+    this.buffer = buffer;
+    this.view = new DataView(buffer);
+  }
 }
+
+class AsepriteView {
+	private buffer: ArrayBuffer;
+	private view: DataView;
+	private offset: number = 0;
+	private constructor(buffer: ArrayBuffer) {
+		this.buffer = buffer;
+		this.view = new DataView(buffer);
+	}
+
+	readByte(): number {
+		const value = this.view.getUint8(this.offset);
+		this.offset += 1;
+		return value;
+	}
+
+	readWord(): number {
+		const value = this.view.getUint16(this.offset, true);
+		this.offset += 2;
+		return value;
+	}
+
+	readShort(): number {
+		const value = this.view.getInt16(this.offset, true);
+		this.offset += 2;
+		return value;
+	}
+
+	readDword(): number {
+		const value = this.view.getUint32(this.offset, true);
+		this.offset += 4;
+		return value;
+	}
+
+	readString(): string {
+		const length = this.readWord();
+		const bytes = new Uint8Array(this.buffer.slice(this.offset, this.offset+length));
+		this.offset += length;
+		return new TextDecoder().decode(bytes);
+	}
+}
+
+//export default AsepriteView.parse; idk if this would be valid I would assume it would lose its context. 
