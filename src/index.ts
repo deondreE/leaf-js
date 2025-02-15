4360dfd1b006c1f6703f00bfbc3638b8c17adf9e
Deondre English <deondre.eldest.english@gmail.com>
2025-02-14 22:52:20 -0600

    added tests for parser, added python for remembering to add it.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './renderer.old';
 export * from './scene';
 export * from './profiler';
+export * from './leaf';
--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,20 @@
-import Renderer from './renderer';
 import './style.css';
 import './leaf';
-import { Model } from './types/scene.types';
+import { ModelTypes } from './Model';
 
-console.log('Main loaded');
-/*
-async function start() {
-  const models: Model[] = [
-    {
-      name: "Square Model",
-      type: "square", // Type can be "square", "circle", or "custom"
-      size: { w: 100, h: 100 },
-      id: 0,
-      position: undefined,
-      verticies: undefined,
-      shaders: [],
-      startAnimation: false,
-      applyTransformation: function (type: 'scale' | 'rotate' | 'translate', value: { x: number; y: number; z: number; }): void {
-        throw new Error('Function not implemented.');
-      }
-    },
-    {
-      name: "Square Model",
-      type: "square", // Type can be "square", "circle", or "custom"
-      size: { w: 100, h: 100 },
-      id: 0,
-      position: undefined,
-      verticies: undefined,
-      shaders: [],
-      startAnimation: false,
-      applyTransformation: function (type: 'scale' | 'rotate' | 'translate', value: { x: number; y: number; z: number; }): void {
-        throw new Error('Function not implemented.');
-      }
-    },
-  ];
-  
-  const renderer = await Renderer.init({})  // Initializes WebGPU and sets up the canvas
+function Scene() {
 
-  renderer.render(); 
+  const Models = [
+    {
+      type: 'Cube',
+      position: {
+        x: 10,
+        y: 10, 
+      },
+      size: {
+        width: 10,
+        height: 10,
+      }
+    }
+  ]    
 }
-
-start();*/
