2e5ef0f3ec235156dd17e136e9f20ebec6f98fef
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2025-02-14 23:45:05 -0500

    I have got some of this mapped. I need to get some of this working to keep going mapping this out.

--- src/parsers/aseprite.ts
+++ src/parsers/aseprite.ts
@@ -1,3 +1,10 @@
+/*
+Parsing ASE. 
+
+Like many parsers the issue to arise becomes size. an ASEprite document may have large amounts of data to load. 
+
+With a limited data pool and the requirement of the document being completely parsed it needs to be 
+*/
 export interface AseHeader {
   size: number; //dword
   magic: number; //word
@@ -47,5 +54,55 @@ export enum AseChunkType {
 	userData = 0x2020, //data to be appended 
 	slice = 0x2022, //slice frame
 	tileset = 0x2023, //tileset chunk
-	
+
+}
+
+/*
+Chunk data.
+
+Each chunk type has a specific schema its data conforms to.
+because chunks and frames describe dynamic content the spec is not a 1 to 1 description of the schema but the data necessary to construct array or dictionaries of information.
+*/
+
+/*
+Color Palettes
+Color Palettes are declared in rgb and rgba however has 3 different schemas
+That being said the legacy variations can be represented by a [3 or 4]Uint8
+*/
+export type AseLegacyPaletteChunkItem = Uint8Array;
+export type AseLegacyPaletteChunk = {
+	size: number, 
+	items: AseLegacyPaletteChunkItem[],
+};
+
+export type AsePaletteChunk = {
+	size: number,
+}
+
+export type AseLayerChunk = {
+	flags: number, //word 1=visible 2=editable 4=lockmovement 8=background 16=prefer-linked-cels 32=The layer should be displayed collapsed 64=The layer is a reference layer
+	type: number, //word 0=Normal (image) 1=group 2=tilemap
+	layer: number, //word numeric indication of layer depths
+	//width: number, //word (currently ignored)
+	//height: number, //word (also ignored)
+	blendMode: number, //word (pixel addition effect)
+	opacity: number, //byte (header must have the opacity flag (1))
+	//[3]byte
+	name: string, //string
+	tilesetIndex?: number, //dword - tileset index (only applicable to groups)
+};
+
+export type AseCelChunk = {
+	layerIndex: number, //word
+	x: number, //short
+	y: number, //short
+	alpha: number, //byte
+	type: number, //word 0-raw image (deprecated) 1-linked cel 2-CompressedImage 3- Compressed Tilemap
+	zIndex: number, //short 0 - default ordering +N - show this cell nlayers later -N show this cell N layers back.
+	//skip [5]bytes
+}
+
+export type AseCelChunkRawImage = AseCelChunk & {
+	width: number, //word 
+	height: number, //word
 }
