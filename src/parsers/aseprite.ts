f0ba5b658b166278dcbaf4a401557f60e7e9733e
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2025-02-15 22:57:24 -0500

    I can parse User Data and Layers now woot woot

--- src/parsers/aseprite.ts
+++ src/parsers/aseprite.ts
@@ -1,6 +1,5 @@
-import { assert } from "../utils";
-import { AseChunkType, AseColorProfile, AseFrame, AseFrameChunk, AseHeader, AsePoint, AseRect } from "./aseprite.types";
-
+import { assert } from '../utils';
+import { AseChunk, AseChunkType, AseColorPalette, AseColorPaletteEntry, AseColorProfile, AseFrame, AseHeader, AseICCProfile, AseLayer, AseLayerBlendMode, AseLayerFlags, AseLayerType, AseLegacyPalette, AsePair, AsePropertyArray, AsePropertyMap, AsePropertyTypes, AseQuad, AseTag, AseTags, AseTriplet, AseUserData } from './aseprite.types';
 
 export class Aseprite {
   private buffer: ArrayBuffer;
@@ -17,6 +16,7 @@ class AsepriteView {
   private buffer: ArrayBuffer;
   private view: DataView;
   private offset: number = 0;
+  private decoder: TextDecoder = new TextDecoder();
   private constructor(buffer: ArrayBuffer) {
     this.buffer = buffer;
     this.view = new DataView(buffer);
@@ -47,109 +47,291 @@ class AsepriteView {
   }
 
   readLong(skip: number = 0): number {
-	const value = this.view.getInt32(this.offset, true);
-	this.offset += 4 + skip;
-	return value;
+    const value = this.view.getInt32(this.offset, true);
+    this.offset += 4 + skip;
+    return value;
   }
 
-  readFixed(skip: number = 0):number{
-	const raw = this.view.getInt32(this.offset, true);
-	this.offset += 4 + skip;
-	return raw / 65536;
+  readFixed(skip: number = 0): number {
+    const raw = this.view.getInt32(this.offset, true);
+    this.offset += 4 + skip;
+    return raw / 65536;
   }
 
-  readFloat(skip: number = 0):number{
-	const raw = this.view.getFloat32(this.offset, true);
-	this.offset += 4 + skip;
-	return raw;
+  readFloat(skip: number = 0): number {
+    const raw = this.view.getFloat32(this.offset, true);
+    this.offset += 4 + skip;
+    return raw;
   }
 
-  readDouble(skip: number = 0):number{
-	const raw = this.view.getFloat64(this.offset, true);
-	this.offset += 8 + skip;
-	return raw;
+  readDouble(skip: number = 0): number {
+    const raw = this.view.getFloat64(this.offset, true);
+    this.offset += 8 + skip;
+    return raw;
   }
 
   readQword(skip: number = 0): bigint {
-	const raw = this.view.getBigUint64(this.offset, true);
-	this.offset += 16 + skip;
-	return raw;
+    const raw = this.view.getBigUint64(this.offset, true);
+    this.offset += 16 + skip;
+    return raw;
   }
 
   readLong64(skip: number = 0): bigint {
-	const raw = this.view.getBigInt64(this.offset, true);
-	this.offset += 16 + skip;
-	return raw;
+    const raw = this.view.getBigInt64(this.offset, true);
+    this.offset += 16 + skip;
+    return raw;
   }
 
-  //spec defines bytes[l] as a read type however this should be split into the necessary specialized types. 
+  /*
+  Pairs - Aseprite uses a variety of value pair types they all parse to a number but require different methods
+  */
+  private readPair(fn: (n?: number) => number, skip: number): AsePair {
+    fn = fn.bind(this);
+    return [fn(), fn(skip)];
+  }
+
+  readBytePair(skip: number = 0): AsePair {
+    return this.readPair(this.readByte, skip);
+  }
+
+  readWordPair(skip: number = 0): AsePair {
+    return this.readPair(this.readWord, skip);
+  }
+
+  readShortPair(skip: number = 0): AsePair {
+    return this.readPair(this.readShort, skip);
+  }
+
+  readLongPair(skip: number = 0): AsePair {
+	return this.readPair(this.readLong, skip);
+  }
+
+  private readTriplet(fn: (n?: number) => number, skip: number): AseTriplet {
+    fn = fn.bind(this);
+    return [fn(), fn(), fn(skip)];
+  }
+
+  readByteTriplet(skip: number = 0): AseTriplet {
+	return this.readTriplet(this.readByte, skip);
+  }
+
+  private readQuad(fn: (n?: number) => number, skip: number): AseQuad {
+    fn = fn.bind(this);
+    return [fn(), fn(), fn(), fn(skip)];
+  }
+
+  readByteQuad(skip: number = 0): AseQuad {
+	return this.readQuad(this.readByte, skip);
+  }
+
+  readLongQuad(skip: number = 0): AseQuad {
+	return this.readQuad(this.readLong, skip);
+  }
+  //spec defines bytes[l] as a read type however this should be split into the necessary specialized types.
 
   readUint8Array(len: number, skip: number = 0): Uint8Array {
-	const raw = new Uint8Array(this.buffer.slice(this.offset, this.offset + len));
-	this.offset += len + skip;
-	return raw;
+    const raw = new Uint8Array(this.buffer.slice(this.offset, this.offset + len));
+    this.offset += len + skip;
+    return raw;
   }
-
+  private decode(len: number, skip: number = 0){
+	return this.decoder.decode(this.readUint8Array(len, skip));
+  }
   readString(skip: number = 0): string {
-    const length = this.readWord();
-    const bytes = new Uint8Array(this.buffer.slice(this.offset, this.offset + length));
-    this.offset += length + skip;
-    return new TextDecoder().decode(bytes);
+    return this.decode(this.readWord(), skip);
   }
-
-
-  
-  
-  
+  readUUID(skip: number = 0): string {
+	return this.decode(16, skip);
+  }
   readHeader(): AseHeader {
-	const header: AseHeader = {
-		size: this.readDword(),
-		magic: this.readWord(),
-		frames: this.readWord(),
-		width: this.readWord(),
-		height: this.readWord(),
-		colorDepth: this.readWord(),
-		flags: this.readDword(),
-		speed: this.readWord(8),
-		paletteEntry: this.readByte(3),
-		colorCount: this.readWord(),
-		pixelWidth: this.readByte(),
-		pixelHeight: this.readByte(),
-		x: this.readShort(),
-		y: this.readShort(),
-		gridWidth: this.readWord(),
-		gridHeight: this.readWord(84),
-	  };
-	  assert(header.magic === 0xA5E0, "Are you sure this is an aseprite file");
-	  return header;
+    const header: AseHeader = {
+      fileSize: this.readDword(),
+      magic: this.readWord(),
+      frames: this.readWord(),
+      size: this.readWordPair(),
+      colorDepth: this.readWord(),
+      flags: this.readDword(),
+      speed: this.readWord(8),
+      paletteEntry: this.readByte(3),
+      colorCount: this.readWord(),
+      pixelSize: this.readBytePair(),
+      location: this.readShortPair(),
+      gridSize: this.readWordPair(84),
+    };
+    assert(header.magic === 0xa5e0, 'Are you sure this is an aseprite file');
+    return header;
   }
-  
+  readLegacyPalette(chunkType: AseLegacyPalette["chunkType"]): AseLegacyPalette {
+	const len = this.readWord();
+	const colors: AseTriplet[] = [];
+	for(let i = 0; i<len; i++){
+		const o = this.readByte(); //I dont think this will be necessary I think this is a way to overload a Byte length by having each packet have a smaller length size
 
+		const colorCount = this.readByte(); //I think thats the purpose but we will see
+		for(let i = 0; i<colorCount; i++){
+			colors.push(this.readByteTriplet());
+		}
+	}
+	return { chunkType, colors };
+  }
+
+  readColorProfile(): AseColorProfile | AseICCProfile {
+	const profileType = this.readWord(2) as 0 | 1 | 2; //skip the flag
+	const gamma = this.readFixed(8); //skip the 8 reserved
+	if(profileType === 2) {
+		const iccSize = this.readDword();
+		const icc = new Uint8Array(this.buffer.slice(this.offset, this.offset+iccSize));
+		this.offset += iccSize;
+		return {chunkType: 0x2007, profileType, gamma, icc};
+	}
+	return {chunkType: 0x2007, profileType, gamma};
+  }
+
+  readColorPalette(): AseColorPalette{
+	const palletSize = this.readDword();
+	const firstIndex = this.readDword();
+	const lastIndex = this.readDword(8);
+	const colors = new Array<AseColorPaletteEntry>(palletSize).fill(null);
+	for(let i = 0; i<palletSize; i++){
+		const hasName = this.readWord() === 1;
+		const color = this.readByteQuad()
+		colors[i] = hasName ? {color, name:this.readString()}:{color};
+	}
+	return {chunkType: 0x2019, firstIndex, lastIndex, colors};
+  }
+  readTags(): AseTags {
+	const len = this.readWord(8);
+	const tags = new Array<AseTag>(len).fill(null);
+	for(let i = 0; i<len; i++){
+		tags[i] = {
+			range: this.readWordPair(),
+			direction: this.readByte(),
+			repeat: this.readWord(6),
+			tagColor: this.readByteTriplet(1),
+			tagName: this.readString()
+		}
+	}
+	return {chunkType: 0x2018, tags};
+  }
+  readPropertyList(): AsePropertyArray {
+	const len = this.readDword();
+	const elementType = this.readWord();
+	const elements = new Array<AsePropertyTypes>(len).fill(null);
+	for(let i = 0; i<len; i++){
+		elements[i] = this.readPropertyValue();
+	}
+	return elements;
+  }
+  readPropertyValue(): AsePropertyTypes {
+	const pType = this.readWord();
+	switch (pType) {
+		case 0x0001: return this.readByte() > 0;
+		case 0x0002:
+		case 0x0003: return this.readByte();
+		case 0x0004: return this.readShort();
+		case 0x0005: return this.readWord();
+		case 0x0006: return this.readLong();
+		case 0x0007: return this.readDword();
+		case 0x0008: return this.readLong64();
+		case 0x0009: return this.readQword();
+		case 0x000A: return this.readFixed();
+		case 0x000B: return this.readFloat();
+		case 0x000C: return this.readDouble();
+		case 0x000D: return this.readString();
+		case 0x000E:
+		case 0x000F: return this.readLongPair();
+		case 0x0010: return this.readLongQuad();
+		case 0x0011: return this.readPropertyList();
+		case 0x0012: return this.readPropertyMap();
+		case 0x0013: return this.readUUID();
+		default:
+			throw new Error("Prop type mismatch");
+	}
+  }
+  readPropertyEntry(): [string, AsePropertyTypes] {
+	const key = this.readString();
+	return [key, this.readPropertyValue()];
+  }
+  readPropertyMap(): AsePropertyMap {
+	const mapByteSize = this.readDword();
+	const len = this.readDword();
+	const props: AsePropertyMap = {};
+	for(let i = 0; i<len; i++){
+		const [key, value] = this.readPropertyEntry();
+		props[key] = value;
+	}
+	return props;
+  }
+  readUserData(): AseUserData {
+	const flags = this.readDword();
+	return {
+		chunkType: 0x2020,
+		text: (flags & 1) === 1 ? this.readString():undefined,
+		color: (flags & 2) === 2 ? this.readByteQuad():undefined,
+		properties: (flags & 4) === 4 ? this.readPropertyMap():undefined
+	}
+  }
+  readLayer(): AseLayer {
+	const flags = this.readWord() as AseLayerFlags;
+	const layerType = this.readWord() as AseLayerType;
+	const layerChildLevel = this.readWord();
+	const layerSize = this.readWordPair();
+	const blendMode = this.readWord() as AseLayerBlendMode;
+	const alpha = this.readByte();
+	const name = this.readString();
+	const tileIndex = layerType === 2 ? this.readDword():-1;
+
+	return {
+		chunkType: 0x2004,
+		flags,
+		layerType,
+		layerChildLevel,
+		layerSize,
+		blendMode,
+		alpha,
+		name,
+		tileIndex
+	}
+  }
+  readChunk(): AseChunk {
+	const chunkSize = this.readDword();
+	const chunkType = this.readWord() as AseChunkType;
+	switch(chunkType){
+		case 0x0004: 
+		case 0x0011: return this.readLegacyPalette(chunkType);
+		case 0x2007: return this.readColorProfile();
+		case 0x2018: return this.readTags();	
+		case 0x2019: return this.readColorPalette();
+		case 0x2020: return this.readUserData();
+		default:
+			console.log(`Chunk 0x${chunkType.toString(16)}`)
+			return {chunkType:0x2017};
+	}
+	
+  }
+
+  readFrame(): AseFrame {
+	const bytes = this.readDword() - 14;
+	const magic = this.readWord(2);
+	const duration = this.readWord(2);
+	const chunkSizeNew = this.readDword();
+	assert(magic === 0xF1FA, "Magic mismatch");
+	//console.log(`Frame has ${bytes} bytes`, chunkSizeNew);
+	var chunks = new Array(chunkSizeNew).fill(null);
+	for(let i = 0; i<chunkSizeNew;i++){
+		chunks[i] = this.readChunk();
+		if(chunks[i].chunkType !== 0x2017) console.log("Chunk", chunks[i])
+	}
+	console.log("Chunks", chunks);
+	return {duration, chunks};
+  }
   static parse(buffer: ArrayBuffer) {
     const v = new AsepriteView(buffer);
-    const header: AseHeader = {
-      size: v.readDword(),
-      magic: v.readWord(),
-      frames: v.readWord(),
-      width: v.readWord(),
-      height: v.readWord(),
-      colorDepth: v.readWord(),
-      flags: v.readDword(),
-      speed: v.readWord(8),
-      paletteEntry: v.readByte(3),
-	  colorCount: v.readWord(),
-      pixelWidth: v.readByte(),
-      pixelHeight: v.readByte(),
-      x: v.readShort(),
-      y: v.readShort(),
-      gridWidth: v.readWord(),
-	  gridHeight: v.readWord(84),
-    };
-	//assert(header.magic === 0xA5E0, `Header magic mismatch ${header.magic} ${0xA5E0}`);
-	console.log(header, v.offset);
-	//v.readFrame();
-	console.log(header, v.offset);
-    
+    const header: AseHeader = v.readHeader();
+    //assert(header.magic === 0xA5E0, `Header magic mismatch ${header.magic} ${0xA5E0}`);
+    console.log(header, v.offset);
+    const frame = v.readFrame();
+    console.log(header, frame, v.offset);
   }
 }
 
--- src/parsers/aseprite.types.ts
+++ src/parsers/aseprite.types.ts
@@ -9,263 +9,139 @@ export interface Aseprite {
   With a limited data pool and the requirement of the document being completely parsed it needs to be 
   */
 export interface AseHeader {
-  size: number; //dword
+  fileSize: number; //dword
   magic: number; //word
   frames: number; //word
-  width: number; //word
-  height: number; //word
+  size: AsePair; //word pair
   colorDepth: number; // 8 | 16 | 32 (word)
   flags: number; //dword
   speed: number;
   paletteEntry: number; //this is a byte
   //read past the next 3 bytes
   colorCount: number; //word 0===256 in old sprites...?
-  pixelWidth: number; //byte
-  pixelHeight: number; //byte
-  x: number; //short
-  y: number; //short
-  gridWidth: number; // word (described as grid with but each cell is square so this represents the size of a cell)
-  gridHeight: number;
+  pixelSize: AsePair; //byte pair
+  location: AsePair; //short pair
+  gridSize: AsePair; //word pair
   //read the next 84 bytes
 }
 
-export interface AseFrame {
-  size: number; //dword (number of bytes in the frame)
-  magic: number; //word should always be 0xF1FA
-  //chunksOld: number; //word (represents the number of chunks in the frame if overflow there are more chunks in the frame that cant be described in this field and should use new field. 0xFFFF is max). not supporting legacy for mvp
-  duration: number; //word (in ms)
-  //skip next byte[2]
-  chunkSize: number; //dword (if 0 check old field)
-  chunks: any[]; //for now
+export type AseFrame = {
+	duration: number; //word
+	chunks: AseChunk[];
 }
 
-export enum AseChunkType {
-  oldPalette1 = 0x0004, //should only be used if newPalette does not also exist.
-  oldPalette2 = 0x0011, //ignore if newPallet exists
-  layer = 0x2004,
-  cel = 0x2005,
-  celExtra = 0x2006,
-  colorProfile = 0x2007,
-  external = 0x2008,
-  mask = 0x2016, //deprecated
-  pth = 0x2017, //never used
-  tags = 0x2018,
-  palette = 0x2019, //newPalette
-  userData = 0x2020, //data to be appended
-  slice = 0x2022, //slice frame
-  tileset = 0x2023, //tileset chunk
+export type AseChunk = AseLegacyPalette | AseColorProfile | AseICCProfile | AsePath | AseColorPalette | AseTags | AseUserData;
+
+export type AseChunkType = AseChunk['chunkType'];
+
+export type AseLegacyPalette = {
+	chunkType: 0x0004 | 0x0011;
+	colors: AseTriplet[];
+}
+export type AseLayerFlags = 1 | 2 | 4 | 8 | 16 | 32 | 64;
+export type AseLayerType = 0 | 1 | 2;
+export enum AseLayerBlendMode {
+	normal = 0,
+	multiply = 1,
+	screen = 2,
+	overlay = 3,
+	darken = 4,
+	lighten = 5,
+	colorDodge = 6,
+	colorBurn = 7,
+	hardLight = 8,
+	softLight = 9,
+	difference = 10,
+	exclusion = 11,
+	hue = 12,
+	saturation = 13,
+	color = 14,
+	lumnosity = 15,
+	addition = 16,
+	subtract = 17,
+	divide = 18
+}
+export type AseLayer = {
+	chunkType: 0x2004;
+	flags: AseLayerFlags;
+	layerType: AseLayerType;
+	layerChildLevel: number;
+	layerSize: AsePair;
+	blendMode: AseLayerBlendMode;
+	alpha: number;
+	name: string;
+	tileIndex: number;
+}
+export type AsePath = {
+	chunkType: 0x2017
 }
 
-export type AseFrameChunk = AseColorProfile | AseChunkUserData | AseLayerChunk | AseCelChunk | AseCelExtraChunk | AseExternalChunk | AseMaskChunk | AsePathChunk;
-
-export type AseFrameChunkBase = {
-  size: number; //dword
-};
+export type AseColorProfileBase = {
+	chunkType: 0x2007;
+	gamma: number; //fixed (the flag will be unecessary for now it will be skipped)
+}
 
 export type AseColorProfile = {
-  type: typeof AseChunkType.colorProfile;
-  profileType: number; //0 no profile, 1 = sRGB, 2 = internal
-  profileFlags: number; //1 = use special fixed gamma
-  gamma: number; //fixed
-  //[8]bytes reserved (should be 0)
-  //if internal
-  profileLength?: number;
-} & AseFrameChunkBase;
+	profileType: 0 | 1; //no profile and sRGB will be treated the same
+} & AseColorProfileBase;
 
-export type AseChunkUserData = {
-  type: typeof AseChunkType.userData;
-  userDataFlags: number; //1 = has text, 2 = has color, 4; assuming bitwise comparison allows for 3 = has text and color 5 = has properties * text and 6 has props and color and 7 has props color and text
-  text?: string; //STRING
-  color?: Uint8Array; //color this should be [4]Uint8.
-  properties?: Record<string, any>;
-} & AseFrameChunkBase;
+export type AseICCProfile = {
+	profileType: 2;
+	icc: Uint8Array;
+} & AseColorProfileBase;
 
-/*
-  Chunk data.
-  
-  Each chunk type has a specific schema its data conforms to.
-  because chunks and frames describe dynamic content the spec is not a 1 to 1 description of the schema but the data necessary to construct array or dictionaries of information.
-  */
-
-/*
-  Color Palettes
-  Color Palettes are declared in rgb and rgba however has 3 different schemas
-  That being said the legacy variations can be represented by a [3 or 4]Uint8
-  */
-export type AseLegacyPaletteChunkItem = Uint8Array;
-export type AseLegacyPaletteChunk = {
-  type: typeof AseChunkType.oldPalette1 | typeof AseChunkType.oldPalette2;
-  paletteSize: number;
-  items: AseLegacyPaletteChunkItem[];
-} & AseFrameChunkBase;
-
-export type AsePaletteChunkItem = {
-	red: number; //byte
-	green: number; //byte
-	blue: number; //byte
-	alpha: number; //byte
-	colorName: string; //string
+export type AseColorPaletteEntry = {
+	color: AseQuad;
+	name?: string
 }
-export type AsePaletteChunk = {
-  paletteSize: number;
-  firstIndex: number;
-  lastIndex: number;
-  //skip 8 bytes
-
-};
-
-export type AseLayerChunk = {
-  type: typeof AseChunkType.layer;
-  flags: number; //word 1=visible 2=editable 4=lockmovement 8=background 16=prefer-linked-cels 32=The layer should be displayed collapsed 64=The layer is a reference layer
-  layerType: number; //word 0=Normal (image) 1=group 2=tilemap
-  layer: number; //word numeric indication of layer depths
-  //width: number, //word (currently ignored)
-  //height: number, //word (also ignored)
-  blendMode: number; //word (pixel addition effect)
-  opacity: number; //byte (header must have the opacity flag (1))
-  //[3]byte
-  name: string; //string
-  tilesetIndex?: number; //dword - tileset index (only applicable to groups)
-} & AseFrameChunkBase;
-
-export type AseCelChunkBase = {
-	type: typeof AseChunkType.cel;
-	layerIndex: number; //word
-  x: number; //short
-  y: number; //short
-  alpha: number; //byte
-  zIndex: number; //short 0 - default ordering +N - show this cell nlayers later -N show this cell N layers back.
-  //skip 5 bits
-} & AseFrameChunkBase;
-
-export type AseCelImage = {
-	celType: 0 | 2;
-	width: number; //word
-	height: number; //word
-	pixels: AsePixel[]
-} & AseCelChunkBase;
-
-export type AseLinkedCel = {
-	celType: 1;
-	frameIndex: number; //word
-} & AseCelChunkBase;
-
-export type AseCelTileMap = {
-	celType: 3;
-	width: number; //word
-	height: number; //word
-	tileSize: number; //word should always be 32bits
-	tileIdMask: number; //DWord
-	tileXFlip: number; //DWord
-	tileYFlip: number; //DWord
-	tileDiagFlip: number; //Dword
-	//byte[10] reserved
-	tiles: number[]; //This should be a fixed type 
-} & AseCelChunkBase;
-
-export type AseCelChunk = AseCelImage | AseLinkedCel | AseCelTileMap;
-
-export type AseCelExtraChunk = {
-	type: typeof AseChunkType.celExtra;
-	flags: number; //DWord 1=precise bounds
-	x: number; //fixed
-	y: number; //fixed
-	width: number; //fixed
-	height: number; //fixed
-	//skip 16 bytes
-} & AseFrameChunkBase;
-
-
-
-export type AseMaskChunk = {
-	x: number; //short
-	y: number; //short
-	width: number; //word
-	height: number; //word
-	//skip 8 bytes
-	name: string; //string
-	bitmap: boolean[]; //size = height*((width+7)/8)
-} & AseFrameChunkBase;
-
-export type AsePathChunk = {
-	type: typeof AseChunkType.pth;
-} & AseFrameChunkBase;
-
-
+export type AseColorPalette = {
+	chunkType: 0x2019;
+	firstIndex: number;
+	lastIndex: number;
+	colors: AseColorPaletteEntry[];
+}
+export enum AseLoopDirection {
+	forward = 0,
+	reverse = 1,
+	pingPong = 2,
+	pingPongReverse = 3
+}
 export type AseTag = {
-	from: number; //word
-	to: number; //word
-	direction: number; //byte 0 forward 1 revers 2 ping-pong 3 ping-pong reverse
-	repeat: number; //word 0 infinite 1 plays once (ping pong single direction) 2 (plays twice (ping-pong it plays once total))
-	//skip 6 bytes
-	tagColor: Uint8Array; //3 bytes
-	//skip byte
-	tagName: string;
+	range: AsePair;
+	direction: AseLoopDirection;
+	repeat: number;
+	tagColor: AseTriplet;
+	tagName: string
 }
-export type AseTagsChunk = {
-	type: typeof AseChunkType.tags
+
+export type AseTags = {
+	chunkType: 0x2018;
 	tags: AseTag[]
-} & AseFrameChunkBase;
-
-export type AseExternalChunkEntry = {
-	entryId: number; //dword
-	type: number; //byte 0 = palette 1 = tileset 2 = extension name for properties 3 = extension name for tile managment (can exists one pers sprite)
-	//skip 7 bytes
-	externalId: string; //string
-};
-
-export type AseExternalChunk = {
-	type: typeof AseChunkType.external;
-	entries: AseExternalChunkEntry;
-} & AseFrameChunkBase
-
-export type ICCProfile = {
-  size: number; //DWord
-  cMMType: number; //DWord
-  versionNumber: number; //DWord
-  deviceClass: number; //DWord
-  colorSpace: number; //DWord
-  connectionSpace: number; //DWord
-  creationDate: number; //TODO Datetype for now [3]DWord
-  acspSignature: number; //DWord
-  primaryPlatformSignature: number; //DWord
-  cMMFlags: number; //DWord
-  deviceManufaturer: number; //DWord
-  deviceAttribute: number; //DWord*2 8Bytes type unknown
-  renderIntent: number; //DWord
-  illuminantChannels: Uint32Array; //Should be 3 Uint32 values
-  profileCreatorSignature: number; //DWord
-  profileID: number; //DWord*4... maybe a short string
-  //skip 28 bytes
-};
-
-
-
-export type AsePoint = Uint8Array; //len 2
-export type AseRect = Uint8Array; //len 4
-export type AsePixel = Uint8Array; //len 1, 2, or 4
-export type AseTile = number; //8,16,or32bit
-export type AseUUID = string; //will be read as string this could just be a uint8 array
-export enum AseUserDataPropertyType {
-  mixed = 0x0000,
-  bool = 0x0001,
-  int8 = 0x0002,
-  uint8 = 0x0003,
-  int16 = 0x0004,
-  uint16 = 0x0005,
-  int32 = 0x0006,
-  uint32 = 0x0007,
-  int64 = 0x0008,
-  uint64 = 0x0009,
-  fixed = 0x000a,
-  float = 0x000b,
-  double = 0x000c,
-  string = 0x000d,
-  point = 0x000e,
-  size = 0x000f,
-  rect = 0x0010,
-  vector = 0x0011,
-  map = 0x0012,
-  uuid = 0x0013,
 }
 
+export type AsePropertyTypers = 0x0001 | 0x0002 | 0x0003 | 0x0004 | 0x0005 | 0x0006 | 0x0008 | 0x0009 | 0x000A | 0x000B | 0x000C | 0x000D | 0x000E | 0x000F | 0x0010 | 0x0011 | 0x0012 | 0x0013;
+
+export type AsePropertyTypes = boolean | number | string | bigint | AsePair | AseQuad | AsePropertyArray | AsePropertyMap
+export type AsePropType<T extends AsePropertyTypers | 0x0 = 0> = T extends 0x0001 ? boolean
+: T extends 0x0002 | 0x0003 | 0x0004 | 0x0005 | 0x0006 | 0x0007 | 0x000A | 0x000B ? number
+: T extends 0x0008 | 0x0009 | 0x000C ? bigint
+: T extends 0x000D | 0x0013 ? string
+: T extends 0x000E | 0x000F ? AsePair
+: T extends 0x0010 ? AseQuad
+: T extends 0x0011 ? AsePropertyArray
+: T extends 0x0012 ? AsePropertyMap
+: AsePropertyTypes;
+
+export interface AsePropertyArray extends Array<AsePropertyTypes> {};
+export interface AsePropertyMap {
+	[key: string]: AsePropertyTypes
+}
+export type AseUserData = {
+	chunkType: 0x2020;
+	text?: string,
+	color?: AseQuad,
+	properties?: AsePropertyMap
+}
+export type AsePair = [number, number];
+export type AseTriplet = [number, number, number];
+export type AseQuad = [number, number, number, number];
