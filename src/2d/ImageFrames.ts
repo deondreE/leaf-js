3d67bfd722348b17a180478cbbc1a7c018168e4b
JamesIrwin3 <139516801+JamesIrwin3@users.noreply.github.com>
2025-03-04 21:14:12 -0500

    Basically I should be able to multiply uvSizes by frame index derived xy

--- src/2d/ImageFrames.ts
+++ src/2d/ImageFrames.ts
@@ -8,7 +8,7 @@ import { Renderable } from "./types";
 export default class ImageFrames implements Renderable {
 	imgPromise: Promise<ImageData>; //When I go to sprite this should be disposed of when bind is called.
 	tileSize: number;
-	
+
 	uvSize: [width:number, height: number] = [1,1];
 	uv: [u: number, v: number] = [0,1];
 	//bindGroup: GPUBindGroup;
@@ -20,11 +20,12 @@ export default class ImageFrames implements Renderable {
 	}
 
 	async register(renderer: Renderer): Promise<void>{
-		//Later this can emit an event like an onMount when a sprite has been added.
+		//Later this can emit an event like an onMount when a sprite has been "registered" with the renderer.
 
 		const img = await this.imgPromise;
 		this.uvSize = [this.tileSize/img.width, this.tileSize/img.height];
-		
+
+		console.log(this.uvSize);
 	}
 
 	render(renderer: Renderer, delta: number): void {
