9ef0987115f5164f630c057fe472bcf949e5228e
Deondre English <deondre.eldest.english@gmail.com>
2025-03-05 23:20:58 -0600

    added platform, changed name of lib

--- core/scripts/build.sh
+++ core/scripts/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zig build-exe src/lib.zig -target wasm32-freestanding -fno-entry --export=add
+zig build-exe src/wasm_dispatcher.zig -target wasm32-freestanding -fno-entry --export=add
