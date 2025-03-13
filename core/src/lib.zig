d1b46fbe4e8076e53f1a203ace69eb5e914d6b67
Deondre English <deondre.eldest.english@gmail.com>
2025-03-12 23:44:32 -0500

    added toml parsing / tokenization; added wasm build script

--- core/src/lib.zig
+++ core/src/lib.zig
@@ -1,8 +1,11 @@
+const std = @import("std");
+const toml = @import("./toml.zig");
+
 extern fn print(u8) void;
 
 /// Dispatchs functions based on numerical input, allowing for parsing to be done inside of zig, rather then inside of Typescript. Returns `void` takes `u8`.
-export fn dispatch(callType: u8) void {
-    switch (callType) {
-        0 => print(u8),
-    }
+export fn dispatch(string: u8) void {
+    print(string);
+    toml.TomlParser.init();
+    toml.TomlParser.parse();
 }
