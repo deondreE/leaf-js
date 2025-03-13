7daf0ca58898842adda3302380361c21ae68909a
deondreE <deondre.eldest.english@gmail.com>
2025-03-13 18:03:05 -0500

    added simple matrix implementation will be replaced by 'glm'

--- core/src/math.zig
+++ core/src/math.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 pub const Vec3 = struct {
     x: f32,
     y: f32,
@@ -10,3 +12,49 @@ pub const Vec4 = struct {
     b: f32,
     a: f32,
 };
+
+pub const Mat4 = struct {
+    data: [4][4]f32 = .{.{0} ** 4} ** 4,
+
+    pub fn create(self: *Mat4) [4][4]f32 {
+        return self.data;
+    }
+
+    /// Multiplies two matricies into a given source obejct.
+    pub fn multiply(self: *Mat4, m1: [4][4]f32, m2: [4][4]f32) [4][4]f32 {
+        for (0..4) |row| {
+            for (0..4) |col| {
+                var sum: f32 = 0;
+                for (0..4) |k| {
+                    sum += m1[row][k] * m2[k][col];
+                }
+                self.data[row][col] = sum;
+            }
+        }
+
+        return self.data;
+    }
+};
+
+test "Matrix Multiplication" {
+    const expect = std.testing.expect;
+
+    const m1: [4][4]f32 = .{
+        .{ 1, 2, 3, 4 },
+        .{ 5, 6, 7, 8 },
+        .{ 9, 10, 11, 12 },
+        .{ 13, 14, 15, 16 },
+    };
+
+    const m2: [4][4]f32 = .{ .{ 17, 18, 19, 20 }, .{ 21, 22, 23, 24 }, .{ 25, 26, 27, 28 }, .{ 29, 30, 31, 32 } };
+
+    const expected_result: [4][4]f32 = .{ .{ 250, 260, 270, 280 }, .{ 618, 644, 670, 696 }, .{ 986, 1028, 1070, 1112 }, .{ 1354, 1412, 1470, 1528 } };
+
+    const result = Mat4.multiply(m1, m2);
+
+    for (0..4) |i| {
+        for (0..4) |j| {
+            try expect(result[i][j] == expected_result[i][j]);
+        }
+    }
+}
---
+++ scripts/ready_build.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+def ready_build():
+    src_file_paths = ["../core/lib.wasm", "../src/style.css"]
+    dist_dir = "dist"
+
+    os.makedirs(dist_dir, exist_ok=True)
+
+    for file in src_file_paths:
+        if os.path.exists(file):
+            shutil.copy(file, dist_dir)
+            print(f"Copied {file} to {dist_dir}")
+        else:
+            print(f"Warning: {file} not found.")
+
+if __name__ == "__main__":
+    ready_build()
